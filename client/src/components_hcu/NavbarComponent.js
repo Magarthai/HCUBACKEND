c98e9fe945c2489fda4958a2afd8ed04fe0828d2
Rawisada <112146634+Rawisada@users.noreply.github.com>
2024-02-02 23:39:52 +0700

    Update navbar admin

--- client/src/components_hcu/NavbarComponent.js
+++ client/src/components_hcu/NavbarComponent.js
@@ -63,7 +63,7 @@ const NavbarComponent = (props) => {
             Dashboard
           </Link>
         </li>
-        <li className="nav-item pr-3 pb-3">
+        <li className="nav-item pr-2 pb-3">
           <Link onClick={handleLogout} className="nav-link" target="_parent">
             ออกจากระบบ
           </Link>
@@ -71,41 +71,41 @@ const NavbarComponent = (props) => {
       </ul>
 
       <ul className="nav flex-column font1" id="navManager">
-        <li className="nav-item pr-3 pb-3">
-          <Link to="/adminQueueManagementSystemComponent" className="nav-link" target="_parent">
+        <li className="nav-item pr-3">
+          <Link to="/adminQueueManagementSystemComponent" className="nav-link" target="_parent" style={{height:"50px"}}>
             ระบบจัดการคิว
           </Link>
         </li>
-        <li className="nav-item pr-3 pb-3">
-          <Link to="/AppointmentManagerComponent" className="nav-link" target="_parent">
+        <li className="nav-item pr-3">
+          <Link to="/AppointmentManagerComponent" className="nav-link" target="_parent" style={{height:"50px"}}>
             ระบบนัดหมาย
           </Link>
         </li>
-        <li className="nav-item pr-3 pb-3">
-          <Link to="/timeTableGeneralAdmin" className="nav-link" target="_parent">
+        <li className="nav-item pr-3">
+          <Link to="/timeTableGeneralAdmin" className="nav-link" target="_parent" style={{height:"50px"}}>
             เวลาเข้าทำการ
           </Link>
         </li>
-        <li className="nav-item pr-3 pb-3">
-          <Link to="/adminActivityTodayComponent" className="nav-link" target="_parent">
+        <li className="nav-item pr-3">
+          <Link to="/adminActivityTodayComponent" className="nav-link" target="_parent" style={{height:"50px"}}>
             กิจกรรม
           </Link>
         </li>
       </ul>
 
       <ul className="nav flex-column font1" id="navInformation">
-        <li className="nav-item pr-3 pb-3">
-          <Link to="#" className="nav-link" target="_parent">
+        <li className="nav-item pr-3">
+          <Link to="#" className="nav-link" target="_parent" style={{height:"50px"}}>
             ข้อมูลทั่วไป
           </Link>
         </li>
-        <li className="nav-item pr-3 pb-3">
-          <Link to="#" className="nav-link" target="_parent">
+        <li className="nav-item pr-3">
+          <Link to="#" className="nav-link" target="_parent" style={{height:"50px"}}>
             ข้อเสนอแนะ
           </Link>
         </li>
-        <li className="nav-item pr-3 pb-3">
-          <Link to="#" className="nav-link" target="_parent">
+        <li className="nav-item pr-3">
+          <Link to="#" className="nav-link" target="_parent" style={{height:"50px"}}>
             คู่มือ
           </Link>
         </li>
