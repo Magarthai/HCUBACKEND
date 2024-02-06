e330531fbb60ec4cd91fd773a65e3048ad6fb76f
Rawisada <112146634+Rawisada@users.noreply.github.com>
2024-02-07 02:27:57 +0700

    Update ui

--- client/src/components_hcu/AppointmentManagerComponent.js
+++ client/src/components_hcu/AppointmentManagerComponent.js
@@ -607,13 +607,16 @@ const AppointmentManagerComponent = (props) => {
     const adminCards = document.querySelectorAll('.admin-appointment-card');
 
     function handleCardClick(event) {
-        adminCards.forEach(card => card.classList.remove('focused'));
-        event.currentTarget.classList.add('focused');
-    }
+        let currentCard = event.currentTarget
+        let isFocused = currentCard.classList.contains('focused')
+        if(isFocused){
+            currentCard.classList.remove('focused');
 
-    adminCards.forEach(card => {
-        card.addEventListener('click', handleCardClick);
-    });
+        }else{
+            adminCards.forEach(card => card.classList.remove('focused'));
+            currentCard.classList.add('focused');
+        }
+    }
 
     const statusElements = document.querySelectorAll('.admin-appointment-status');
 
@@ -717,7 +720,7 @@ const AppointmentManagerComponent = (props) => {
                                 </div>
                                 <div className="admin-appointment-box-card">
                                     {AppointmentUsersData.sort((a, b) => a.timeslot.start.localeCompare(b.timeslot.start)).map((AppointmentUserData, index) => (
-                                        <div className="admin-appointment-card colorPrimary-800" key={index}>
+                                        <div className="admin-appointment-card colorPrimary-800" key={index} onClick={handleCardClick}>
                                             <div className="admin-appointment-card-time admin-textBody-small" onClick={() => openDetailAppointment(AppointmentUserData)}>
                                                 {AppointmentUserData.timeslot.start}-{AppointmentUserData.timeslot.end}
                                             </div>
